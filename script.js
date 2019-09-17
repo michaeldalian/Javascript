f171a0a3ae086cde8324b6ead56fb82c99fd4771
dalian <michael.dalian@gmail.com>
2019-09-17 14:34:38 +0200

    V1 ok

--- script.js
+++ script.js
@@ -2,18 +2,40 @@ const main = () => {
   // document.getElementById("nbAle").value = "00";
 
   let _monNombre;
+  let _monText;
+  let _found = false;
+  const stateTable = [1, "Trouvé", 2, "Plus petit", 3, "plus grand"];
 
   //compris entre 0 et1 *100 arrondi
   const _PcNombre = Math.round(Math.random() * 100);
 
-  function myfunc() {
-    _monNombre = window.prompt("Devine combien j'ai de doigts");
+  //   function fnPrint(t) {
+  //     if (_found) {
+  //       Alert(t);
+  //     } else {
+  //       _monNombre = window.prompt(t);
+  //     }
+  //   }
+
+  function testNum(a) {
+    if (a === _PcNombre) {
+      _found = true;
+    } else {
+      _found = false;
+    }
   }
 
-  for (let i = 0; i < 2; i++) {
-    myfunc();
-    // Sortie de boucle
-    return;
+  for (let i = 3; i > 1; i--) {
+    if (_found) {
+      _monText = `Bravo! Le nombre était ${_PcNombre}`;
+      Alert(_monText);
+      // Sortie de boucle
+      return;
+    } else {
+      _monText = `Perdu! Rééssaye ( ${i} chances)`;
+      _monNombre = window.prompt(_monText);
+      testNum(_monNombre);
+    }
   }
 
   // window.prompt("Devine combien j'ai de doigts", _monNombre);
