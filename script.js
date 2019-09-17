3870e44ab0898d519bf1e4616ba54fb50ac55663
dalian <michael.dalian@gmail.com>
2019-09-17 14:54:34 +0200

    Nouveau test

--- script.js
+++ script.js
@@ -1,46 +1,57 @@
 const main = () => {
-  // document.getElementById("nbAle").value = "00";
+  // A changer manuellement pour choisir son test
+  const TestAEffectuer = 2;
+  switch (TestAEffectuer) {
+    case 1: // Test1
+      let _monNombre;
+      let _monText;
+      let _found = false;
+      const lang = "fr";
 
-  let _monNombre;
-  let _monText;
-  let _found = false;
-  const stateTable = [1, "Trouvé", 2, "Plus petit", 3, "plus grand"];
+      //   Essai localisation
+      //const stateTable = { fr: ["Trouvé", "Plus petit", "plus grand"], en: [] };
+      //console.log(stateTable.lang[0]);
 
-  //compris entre 0 et1 *100 arrondi
-  const _PcNombre = Math.round(Math.random() * 100);
+      //compris entre 0 et1 *100 arrondi
+      const _PcNombre = Math.round(Math.random() * 100);
 
-  //   function fnPrint(t) {
-  //     if (_found) {
-  //       Alert(t);
-  //     } else {
-  //       _monNombre = window.prompt(t);
-  //     }
-  //   }
+      //   function fnPrint(t) {
+      //     if (_found) {
+      //       Alert(t);
+      //     } else {
+      //       _monNombre = window.prompt(t);
+      //     }
+      //   }
 
-  function testNum(a) {
-    if (a === _PcNombre) {
-      _found = true;
-    } else {
-      _found = false;
-    }
+      function testNum(a) {
+        if (a === _PcNombre) {
+          _found = true;
+        } else {
+          _found = false;
+        }
+      }
+
+      for (let i = 3; i > 1; i--) {
+        if (_found) {
+          _monText = `Bravo! Le nombre était ${_PcNombre}`;
+          Alert(_monText);
+          // Sortie de boucle
+          return;
+        } else {
+          _monText = `Perdu! Rééssaye ( ${i} chances)`;
+          _monNombre = window.prompt(_monText);
+          testNum(_monNombre);
+        }
+      }
+
+      // window.prompt("Devine combien j'ai de doigts", _monNombre);
+
+      console.log(_monNombre);
+
+    case 2: // Test1
+
+    default:
   }
-
-  for (let i = 3; i > 1; i--) {
-    if (_found) {
-      _monText = `Bravo! Le nombre était ${_PcNombre}`;
-      Alert(_monText);
-      // Sortie de boucle
-      return;
-    } else {
-      _monText = `Perdu! Rééssaye ( ${i} chances)`;
-      _monNombre = window.prompt(_monText);
-      testNum(_monNombre);
-    }
-  }
-
-  // window.prompt("Devine combien j'ai de doigts", _monNombre);
-
-  console.log(_monNombre);
 };
 
 main();
